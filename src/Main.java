0f62e940f0a1e63668f362fe7721f337faaafb60
Frokolll111 <buther1433@gmail.com>
2023-03-14 21:26:03 +0700

    Сделал задачу из билета 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(isPalindrome("Анна"));
+        System.out.println(isPalindrome("Мотор"));
     }
+
+    public static boolean isPalindrome(String text) {
+        return text.equalsIgnoreCase(new StringBuffer(text).reverse().toString());
+    }
+
 }
